388c38d9da888b02bf9dc749476d82fe4de2363f
Dima Samoshkin <de.metro.samuel@gmail.com>
2022-11-05 17:54:55 +0400

    Добавлена маршрутизация по техническому заданию

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,11 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute} from '../../const';
 import MainScreen from '../../pages/main-screen/main-screen';
+import MoviePageScreen from '../../pages/movie-page-screen.tsx/movie-page-screen';
+import MyListScreen from '../../pages/my-list-screen/my-list-screen';
+import PlayerScreen from '../../pages/player-screen/player-screen';
+import AddReviewScreen from '../../pages/add-review-screen/add-review-screen';
+import SignInScreen from '../../pages/sign-in-screen/sign-in-screen';
 
 type AppScreenProp = {
   title: string;
@@ -8,7 +15,40 @@ type AppScreenProp = {
 
 function App({title, genre, year}: AppScreenProp): JSX.Element {
   return (
-    <MainScreen title={title} genre={genre} year={year}/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={
+            <MainScreen
+              title={title}
+              genre={genre}
+              year={year}
+            />
+          }
+        />
+        <Route
+          path={AppRoute.Film}
+          element={<MoviePageScreen />}
+        />
+        <Route
+          path={AppRoute.Review}
+          element={<AddReviewScreen />}
+        />
+        <Route
+          path={AppRoute.Player}
+          element={<PlayerScreen />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<SignInScreen />}
+        />
+        <Route
+          path={AppRoute.MyList}
+          element={<MyListScreen />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ project/src/const.ts
@@ -0,0 +1,8 @@
+export enum AppRoute {
+  Film = '/films/:id',
+  Login = '/login',
+  Main = '/',
+  MyList = '/mylist',
+  Player = '/player/:id',
+  Review = '/films/:id/review'
+}
