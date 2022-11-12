5b4c099edbbd067eb96e9f5f83ab59944dc9f5e8
Dima Samoshkin <de.metro.samuel@gmail.com>
2022-11-12 17:38:09 +0400

    Удален лишний код в комменте. Похожие фильмы будут сгенерированы позже

--- project/src/pages/movie-page-screen.tsx/movie-page-screen.tsx
+++ project/src/pages/movie-page-screen.tsx/movie-page-screen.tsx
@@ -66,12 +66,6 @@ function MoviePageScreen({movies, comments}: MoviePageScreenProps): JSX.Element 
                   </svg>
                   <span>Play</span>
                 </Link>
-                {/* <button className="btn btn--play film-card__button" type="button">
-                  <svg viewBox="0 0 19 19" width="19" height="19">
-                    <use xlinkHref="#play-s" />
-                  </svg>
-                  <span>Play</span>
-                </button> */}
                 <button className="btn btn--list film-card__button" type="button">
                   <svg viewBox="0 0 19 20" width="19" height="20">
                     <use xlinkHref="#add" />
