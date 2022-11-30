b3d045709e4b76c3d223b5b8b375e0ec2d87ebae
Dima Samoshkin <de.metro.samuel@gmail.com>
2022-11-30 13:06:51 +0400

    Копонент SignIn собирает информацию с форм и отправляет на сервер

--- project/src/pages/sign-in-screen/sign-in-screen.tsx
+++ project/src/pages/sign-in-screen/sign-in-screen.tsx
@@ -1,8 +1,32 @@
+import {AuthData} from '../../types/user';
 import {Helmet} from 'react-helmet-async';
 import Logo from '../../components/logo/logo';
+import {loginAction} from '../../services/api-actions';
 import {LogoPositionClass} from '../../const';
+import {useAppDispatch} from '../../hooks';
+import {FormEvent, useRef} from 'react';
 
 function SignInScreen(): JSX.Element {
+  const loginRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+
+  const dispatch = useAppDispatch();
+
+  const onSubmit = (authData: AuthData) => {
+    dispatch(loginAction(authData));
+  };
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    if (loginRef.current !== null && passwordRef.current !== null) {
+      onSubmit({
+        login: loginRef.current.value,
+        password: passwordRef.current.value,
+      });
+    }
+  };
+
   return (
     <div className="user-page">
       <Helmet>
@@ -13,10 +37,15 @@ function SignInScreen(): JSX.Element {
         <h1 className="page-title user-page__title">Sign in</h1>
       </header>
       <div className="sign-in user-page__content">
-        <form action="#" className="sign-in__form">
+        <form
+          action=""
+          className="sign-in__form"
+          onSubmit={handleSubmit}
+        >
           <div className="sign-in__fields">
             <div className="sign-in__field">
               <input
+                ref={loginRef}
                 className="sign-in__input"
                 type="email"
                 placeholder="Email address"
@@ -27,11 +56,12 @@ function SignInScreen(): JSX.Element {
                 className="sign-in__label visually-hidden"
                 htmlFor="user-email"
               >
-            Email address
+                Email address
               </label>
             </div>
             <div className="sign-in__field">
               <input
+                ref={passwordRef}
                 className="sign-in__input"
                 type="password"
                 placeholder="Password"
@@ -48,7 +78,7 @@ function SignInScreen(): JSX.Element {
           </div>
           <div className="sign-in__submit">
             <button className="sign-in__btn" type="submit">
-          Sign in
+              Sign in
             </button>
           </div>
         </form>
